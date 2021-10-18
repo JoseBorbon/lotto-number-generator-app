192ab0935750e736fc0fb936b2ee2255df217113
JoseBorbon <joserborbon@gmail.com>
2021-10-18 06:48:44 -0400

    added components to App JS file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
-
+import LottoNumbers from './LottoNumbers';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>
+        <LottoNumbers />
+        <LottoNumbers title="Mini Daily" numQuantity={4} maxNum={10} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
